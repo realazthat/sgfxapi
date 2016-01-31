481c0c2d646d6c3c9b9458a02f55faa4138e75ba
realazthat <realazthat@gmail.com>
2016-01-31 04:17:08 -0500

    Updated copy-msys-dlls-to-Debug64.sh to copy glew dll

--- scripts/copy-msys-dlls-to-Debug64.sh
+++ scripts/copy-msys-dlls-to-Debug64.sh
@@ -5,4 +5,5 @@ cp /mingw64/bin/zlib1.dll ./Debug64/.
 cp /mingw64/bin/libwinpthread-1.dll ./Debug64/.
 cp /mingw64/bin/libgcc_s_seh-1.dll ./Debug64/.
 cp /mingw64/bin/libstdc++-6.dll ./Debug64/.
+cp /mingw64/bin/glew32.dll ./Debug64/.
 
